b535370b1f7d5de004b4633aa52b5ec123503067
Selin <selincarbone@outlook.com>
2025-02-20 10:50:35 +0100

    layout the foundation of the project

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Hamburgarmeny
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navLinks = document.querySelector(".nav-links");
+  
+    menuToggle.addEventListener("click", function () {
+      navLinks.classList.toggle("show");
+    });
+  });
+  
